27f0ff67f95ae55381eef17332d8f4908fe418ee
besit06 <behemoth2054@gmail.com>
2024-08-01 12:14:47 +0200

    js04

--- js/task-2.js
+++ js/task-2.js
@@ -4,7 +4,6 @@ function calcAverageCalories(days) {
     for (let i = 0; i < days.length; i++) {
         totalCal += days[i].calories;
     }
-    
     return days.length === 0 ? 0 : totalCal / days.length;
 }
 
